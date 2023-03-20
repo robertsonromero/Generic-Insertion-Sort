53b567f050060d424a768dc34bc0ce9929d423fa
Robertson <122113210+robertsonromero@users.noreply.github.com>
2023-03-20 12:38:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,15 +4,6 @@
 
 int main(int argc, char** argv) {
 	
-	/* You can assume command-line arguments are always correct:
-		e.g., ./a.out filename.txt %d
-		The file doesn't necessarily exist, though.
-		The format is always one of these two: %d, or %f.
-	
-		With testing file provided, your command should be:
-		./a.out testint %d
-	
-	*/
 	
 
 	size_t len = 0;
